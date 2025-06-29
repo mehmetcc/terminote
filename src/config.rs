211df2e413f2efdb9ac58ff1acef9a8eb03c4e43
mehmetcc <mehmetccm@gmail.com>
2025-06-29 15:23:37 +0300

    Create config.rs

---
+++ src/config.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct StoreSettings {
+    pub path: String,
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Settings {
+    pub store: StoreSettings,
+}
+
+impl Settings {
+    pub fn new(config_file: &str) -> Result<Self, ConfigError> {
+        // Build the config from the specified file (e.g. "Settings.toml")
+        let builder = Config::builder().add_source(File::with_name(config_file));
+
+        let config = builder.build()?;
+        config.try_deserialize()
+    }
+
+    pub fn db_path(&self) -> PathBuf {
+        PathBuf::from(&self.store.path).join(&self.store.name)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::error::Error;
 
+use rusqlite::Connection;
+
+mod config;
 mod note;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Create the data folder, if it doesn't exist
-    std::fs::create_dir_all("data")?;
-    let connection = rusqlite::Connection::open("data/notes.db")?;
-    let client = note::NoteClient::new(connection)?;
+    let settings = config::Settings::new("Settings.toml")?;
+    let db_path = settings.db_path();
+    let conn = Connection::open(&db_path)?;
+    let client = note::NoteClient::new(conn)?;
 
+    // Example usage:
     let my_first_note = note::Note::new("My First Note", "This is the content of my first note.");
     let mut my_second_note =
         note::Note::new("My Second Note", "This is the content of my second note.");
@@ -18,6 +22,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     my_second_note.content = "Updated content for my second note.".to_string();
     client.update_note(&mut my_second_note)?;
 
-    // everything succeeded
     Ok(())
 }
